50e30a7fa2dec3b464a2cb1a8a5bb3850787bf90
phonex34 <peon.phonex@gmail.com>
2014-01-08 18:39:43 +0700

    Add event change color buttons & handle

--- ArtCameraPro/src/hust/team7/artcamerapro/EditActivity.java
+++ ArtCameraPro/src/hust/team7/artcamerapro/EditActivity.java
@@ -1,7 +1,11 @@
-	package hust.team7.artcamerapro;
+package hust.team7.artcamerapro;
 
-import hust.team7.actionfilter.BlurFilterAction;
+import hust.team7.actionfilter.GainFilterAction;
 import hust.team7.actionfilter.GaussianFilterAction;
+import hust.team7.actionfilter.GrayscaleFilterAction;
+import hust.team7.actionfilter.InvertFilterAction;
+import hust.team7.actionfilter.RGBAdjustFilterAction;
+import hust.team7.actionfilter.ThresholdFilterAction;
 
 import java.io.File;
 
@@ -22,31 +26,46 @@ import android.view.animation.AnimationUtils;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class EditActivity extends Activity implements OnClickListener {
 
 	private ImageButton btPointilize;
 	private ImageButton btGaussian;
-	private ImageButton btBlur;
-	private ImageButton btMoreConfig;
+	// private ImageButton btBlur;
+
+	private ImageButton btSelectColor;
+	private ImageButton btOriginal;
+	private ImageButton btWarmVintage;
+	private ImageButton btColdVintage;
+	private ImageButton btInvert;
+	private ImageButton btGrayscale;
+	private ImageButton btGain;
+	private ImageButton btThreshold1950;
+	private ImageButton btContBright;
+	
+	private RelativeLayout svContBright;
 	private ImageButton btSelectEffect;
 	private HorizontalScrollView svListEffect;
+	private HorizontalScrollView svListColor;
 	private ImageView ivEditImage;
 	private Bitmap bmpImage, bmpResult;
 	private int check;
 	private String file_name;
 	private Boolean checkSelect = true;
+	private int aCheckFlag;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit);
+		aCheckFlag = 0;
 		initView();
 	}
-	
-	public  Bitmap rotateBitmap(String fileName) {
+
+	public Bitmap rotateBitmap(String fileName) {
 
 		Bitmap bitmap = null;
 		Bitmap rotatedBitmap = null;
@@ -130,32 +149,55 @@ public class EditActivity extends Activity implements OnClickListener {
 		btPointilize.setOnClickListener(this);
 		btGaussian = (ImageButton) findViewById(R.id.btGaussian);
 		btGaussian.setOnClickListener(this);
-		btBlur = (ImageButton) findViewById(R.id.btBlur);
-		btBlur.setOnClickListener(this);
+		// btBlur = (ImageButton) findViewById(R.id.btBlur);
+		// btBlur.setOnClickListener(this);
+
+		btOriginal = (ImageButton) findViewById(R.id.btOriginal);
+		btOriginal.setOnClickListener(this);
+		btWarmVintage = (ImageButton) findViewById(R.id.btWarmVintage);
+		btWarmVintage.setOnClickListener(this);
+		btColdVintage = (ImageButton) findViewById(R.id.btColdVintage);
+		btColdVintage.setOnClickListener(this);
+		btInvert = (ImageButton) findViewById(R.id.btInvert);
+		btInvert.setOnClickListener(this);
+		btGrayscale = (ImageButton) findViewById(R.id.btGrayscale);
+		btGrayscale.setOnClickListener(this);
+		btGain = (ImageButton) findViewById(R.id.btGain);
+		btGain.setOnClickListener(this);
+		btThreshold1950 = (ImageButton) findViewById(R.id.btThreshold1950);
+		btThreshold1950.setOnClickListener(this);
+		
+		btContBright =	(ImageButton) findViewById(R.id.btContBright);
+		btContBright.setOnClickListener(this);
 		btSelectEffect = (ImageButton) findViewById(R.id.btSelectEffect);
 		btSelectEffect.setOnClickListener(this);
+		btSelectColor = (ImageButton) findViewById(R.id.btSelectColor);
+		btSelectColor.setOnClickListener(this);
 		svListEffect = (HorizontalScrollView) findViewById(R.id.svListEffect);
-		ivEditImage = (ImageView)findViewById(R.id.ivImageEdit);
+		svListColor = (HorizontalScrollView) findViewById(R.id.svListColor);
+		ivEditImage = (ImageView) findViewById(R.id.ivImageEdit);
 
+		svContBright =(RelativeLayout) findViewById(R.id.svListContBright);
 		Intent i = getIntent();
 		file_name = i.getStringExtra("image_name");
-		//bmpImage = BitmapFactory.decodeFile(file_name);
-		bmpImage = rotateBitmap(file_name);
-//		File tempPhoto = new File(file_name);
-//		try {
-//			if (!tempPhoto.exists()) {
-//				tempPhoto.createNewFile();
-//			}
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-		
+		bmpImage = BitmapFactory.decodeFile(file_name);
+		bmpResult = bmpImage;
+		// bmpImage = rotateBitmap(file_name);
+		// File tempPhoto = new File(file_name);
+		// try {
+		// if (!tempPhoto.exists()) {
+		// tempPhoto.createNewFile();
+		// }
+		// } catch (IOException e) {
+		// e.printStackTrace();
+		// }
+
 		// to do check null
-		if(bmpImage != null){
+		if (bmpImage != null) {
 			ivEditImage.setImageBitmap(bmpImage);
-			//ivEditImage.setImageDrawable(bmp);
+			// ivEditImage.setImageDrawable(bmp);
 		}
-		
+
 		// Bitmap bmp = ((BitmapDrawable)imgView.getBackground()).getBitmap();
 	}
 
@@ -164,18 +206,20 @@ public class EditActivity extends Activity implements OnClickListener {
 	Onlick check
 	@param view 
 	@author 7-A Nguyen Hai Duong
-	*/
+	 */
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
+		Animation bottomUp = AnimationUtils.loadAnimation(getBaseContext(),
+				R.anim.bottom_up);
+		Animation bottomDown = AnimationUtils.loadAnimation(getBaseContext(),
+				R.anim.bottom_down);
+
 		switch (v.getId()) {
 		case R.id.btPointilize:
-			check = 1;
 
 			break;
 
 		case R.id.btGaussian:
-			check = 2;
-			btMoreConfig.setVisibility(View.VISIBLE);
 			if (bmpImage != null) {
 				GaussianFilterAction gaussAction = new GaussianFilterAction(
 						bmpImage);
@@ -186,38 +230,170 @@ public class EditActivity extends Activity implements OnClickListener {
 						.show();
 			}
 			break;
-
-		case R.id.btBlur:
-			check = 3;
-			btMoreConfig.setVisibility(View.VISIBLE);
+			
+		case R.id.btOriginal:
 			if (bmpImage != null) {
-				BlurFilterAction blurAction = new BlurFilterAction(bmpImage);
-				bmpResult = blurAction.action();
+				ivEditImage.setImageBitmap(bmpImage);
+			} else {
+				Toast.makeText(this, "Null bmpImage cmnr", Toast.LENGTH_SHORT)
+						.show();
+			}
+			break;
+		case R.id.btWarmVintage:
+			if (bmpImage != null) {
+				RGBAdjustFilterAction warmFilter = new RGBAdjustFilterAction(
+						bmpImage, 0.83f, 0.18f, -0.03f);
+				bmpResult = warmFilter.action();
 				ivEditImage.setImageBitmap(bmpResult);
 			} else {
 				Toast.makeText(this, "Null bmpImage cmnr", Toast.LENGTH_SHORT)
 						.show();
 			}
 			break;
+		case R.id.btColdVintage:
+			if (bmpImage != null) {
+				RGBAdjustFilterAction coldFilter = new RGBAdjustFilterAction(
+						bmpImage, -0.09f, 0.06f, 0.35f);
+				bmpResult = coldFilter.action();
+				ivEditImage.setImageBitmap(bmpResult);
+			} else {
+				Toast.makeText(this, "Null bmpImage cmnr", Toast.LENGTH_SHORT)
+						.show();
+			}
+			break;
+		case R.id.btInvert:
+			if (bmpImage != null) {
+				InvertFilterAction invertFilter = new InvertFilterAction(
+						bmpImage);
+				bmpResult = invertFilter.action();
+				ivEditImage.setImageBitmap(bmpResult);
+			} else {
+				Toast.makeText(this, "Null bmpImage cmnr", Toast.LENGTH_SHORT)
+						.show();
+			}
+			break;
+
+		case R.id.btGrayscale:
+			if (bmpImage != null) {
+				GrayscaleFilterAction grayscaleFilter = new GrayscaleFilterAction(
+						bmpImage);
+				bmpResult = grayscaleFilter.action();
+				ivEditImage.setImageBitmap(bmpResult);
+			} else {
+				Toast.makeText(this, "Null bmpImage cmnr", Toast.LENGTH_SHORT)
+						.show();
+			}
+			break;
+			
+		case R.id.btGain:
+			if (bmpImage != null) {
+				GainFilterAction gainFilter = new GainFilterAction(bmpImage);
+				bmpResult = gainFilter.action();
+				ivEditImage.setImageBitmap(bmpResult);
+			} else {
+				Toast.makeText(this, "Null bmpImage cmnr", Toast.LENGTH_SHORT)
+						.show();
+			}
+			break;
+			
+		case R.id.btThreshold1950:
+			if (bmpImage != null) {
+				//GainFilterAction gainFilter = new GainFilterAction(bmpImage);
+				ThresholdFilterAction thresFilter1950 = new ThresholdFilterAction(bmpImage, 92, 150);
+				bmpResult = thresFilter1950.action();
+				ivEditImage.setImageBitmap(bmpResult);
+			} else {
+				Toast.makeText(this, "Null bmpImage cmnr", Toast.LENGTH_SHORT)
+						.show();
+			}
+			break;
+			
 		case R.id.btSelectEffect:
-			if (checkSelect) {
-				Animation bottomUp = AnimationUtils.loadAnimation(getBaseContext(), R.anim.bottom_up);
+
+			switch (aCheckFlag) {
+
+			case 0:
 				svListEffect.setAnimation(bottomUp);
 				svListEffect.setVisibility(View.VISIBLE);
-				checkSelect = false;
-			}
-
-			else {
-				Animation bottomDown = AnimationUtils.loadAnimation(getBaseContext(), R.anim.bottom_down);
+				aCheckFlag = 2;
+				break;
+			case 1:
+				svListColor.setAnimation(bottomDown);
+				svListColor.setVisibility(View.INVISIBLE);
+				svListEffect.setAnimation(bottomUp);
+				svListEffect.setVisibility(View.VISIBLE);
+				aCheckFlag = 2;
+				break;
+			case 2:
 				svListEffect.setAnimation(bottomDown);
-				
 				svListEffect.setVisibility(View.INVISIBLE);
-				checkSelect = true;
+				aCheckFlag = 0;
+				break;
+			case 3:
+				break;
 			}
-			/*
-			 * case R.id.btMoreConfig: switch (check) { case 1: break; case 2:
-			 * break; case 3: break; }
-			 */
+			break;
+		case R.id.btSelectColor:
+			switch (aCheckFlag) {
+
+			case 0:
+				svListColor.setAnimation(bottomUp);
+				svListColor.setVisibility(View.VISIBLE);
+				aCheckFlag = 1;
+
+				break;
+			case 1:
+				svListColor.setAnimation(bottomDown);
+				svListColor.setVisibility(View.INVISIBLE);
+
+				aCheckFlag = 0;
+				break;
+			case 2:
+				svListEffect.setAnimation(bottomDown);
+				svListEffect.setVisibility(View.INVISIBLE);
+				svListColor.setAnimation(bottomUp);
+				svListColor.setVisibility(View.VISIBLE);
+				aCheckFlag = 1;
+				break;
+			case 3:
+				break;
+			}
+			break;
+			
+			
+		case R.id.btContBright:	
+			
+			svContBright.setAnimation(bottomDown);
+			svContBright.setVisibility(View.VISIBLE);
+			break;
+		/*
+		 * case R.id.btSelectEffect: if (checkSelect) {
+		 * 
+		 * svListEffect.setAnimation(bottomUp);
+		 * svListEffect.setVisibility(View.VISIBLE); checkSelect = false; }
+		 * 
+		 * else {
+		 * 
+		 * svListEffect.setAnimation(bottomDown);
+		 * 
+		 * svListEffect.setVisibility(View.INVISIBLE); checkSelect = true; }
+		 * break;
+		 */
+
+		/*
+		 * case R.id.btBlur: check = 3;
+		 * btMoreConfig.setVisibility(View.VISIBLE); if (bmpImage != null) {
+		 * BlurFilterAction blurAction = new BlurFilterAction(bmpImage);
+		 * bmpResult = blurAction.action();
+		 * ivEditImage.setImageBitmap(bmpResult); } else { Toast.makeText(this,
+		 * "Null bmpImage cmnr", Toast.LENGTH_SHORT) .show(); } break;
+		 */
+		// backup phonex
+
+		/*
+		 * case R.id.btMoreConfig: switch (check) { case 1: break; case 2:
+		 * break; case 3: break; }
+		 */
 		}
 	}
 }
