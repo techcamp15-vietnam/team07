794fbe461e9406e087176830bd81035d59bebe90
phonex34 <peon.phonex@gmail.com>
2014-01-08 18:55:28 +0700

    Add handle to RGB effect

---
+++ ArtCameraPro/src/hust/team7/actionfilter/RGBAdjustFilterAction.java
@@ -0,0 +1,42 @@
+package hust.team7.actionfilter;
+
+import hust.team7.filter.RGBAdjustFilter;
+import hust.team7.filter.util.AndroidUtils;
+import android.graphics.Bitmap;
+
+public class RGBAdjustFilterAction {
+
+	private Bitmap bmp;
+	private int width;
+	private int height;
+	private float rfactorValue;
+	private float gfactorValue;
+	private float bfactorValue;
+	private int[] colors;
+
+	// private ImageView ivEditImage;
+
+	public RGBAdjustFilterAction(Bitmap bmp, float rfactorValue,
+			float gfactorValue, float bfactorValue) {
+		this.bmp = bmp;
+		this.width = bmp.getWidth();
+		this.height = bmp.getHeight();
+		this.rfactorValue = rfactorValue;
+		this.gfactorValue = gfactorValue;
+		this.bfactorValue = bfactorValue;
+	}
+
+	public Bitmap action() {
+		colors = AndroidUtils.bitmapToIntArray(this.bmp);
+		RGBAdjustFilter filter = new RGBAdjustFilter();
+		filter.setRFactor(rfactorValue);
+		filter.setGFactor(gfactorValue);
+		filter.setBFactor(bfactorValue);
+		colors = filter.filter(colors, width, height);
+		bmp = Bitmap.createBitmap(colors, 0, width, width, height,
+				Bitmap.Config.ARGB_8888);
+		return bmp;
+
+	}
+
+}
