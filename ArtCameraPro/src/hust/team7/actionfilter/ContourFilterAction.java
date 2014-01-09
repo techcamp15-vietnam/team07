79f824076b9f7d7223338b82a092c670e774a693
theBlackWolf7411 <tanbqhedspik55@gmail.com>
2014-01-09 09:21:18 +0700

    Handler contour effect

--- ArtCameraPro/src/hust/team7/actionfilter/ContourFilterAction.java
+++ ArtCameraPro/src/hust/team7/actionfilter/ContourFilterAction.java
@@ -1,31 +1,52 @@
 package hust.team7.actionfilter;
 
+
 import hust.team7.filter.ContourFilter;
 import hust.team7.filter.util.AndroidUtils;
 import android.graphics.Bitmap;
 
 public class ContourFilterAction {
+
 	private Bitmap bmp;
-	private float levelValue = 15f;
-	private float offsetValue = 0.78f;
-	private float scaleValue = 0.24f;
-	private int width, height;
+	private int width;
+	private int height;
 	private int[] colors;
-	public ContourFilterAction(Bitmap bmp) {
+	private int levelValue;
+	private float offsetValue;
+	private float scaleValue;
+	
+	/**
+	Constructor ContourFilterAction
+	@param bmp, levelValue, offsetValue, scaleValue
+	@author 7-A Bui Quang Tan
+	*/
+	public ContourFilterAction(Bitmap bmp, int levelValue, float offsetValue, float scaleValue) {
 		this.bmp = bmp;
 		this.width = bmp.getWidth();
 		this.height = bmp.getHeight();
-		this.colors = AndroidUtils.bitmapToIntArray(this.bmp);
+		this.levelValue = levelValue;
+		this.offsetValue = offsetValue;
+		this.scaleValue = scaleValue;
 	}
 	
+	/**
+	Handler Contour Effect
+	@param no param
+	@author 7-A Bui Quang Tan
+	*/
 	public Bitmap action() {
+		colors = AndroidUtils.bitmapToIntArray(this.bmp);
 		ContourFilter filter = new ContourFilter();
 		filter.setLevels(levelValue);
-		filter.setScale(scaleValue);
 		filter.setOffset(offsetValue);
+		filter.setScale(scaleValue);
 		colors = filter.filter(colors, width, height);
 		bmp = Bitmap.createBitmap(colors, 0, width, width, height,
 				Bitmap.Config.ARGB_8888);
 		return bmp;
+
 	}
+
 }
+
+
