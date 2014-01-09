51615dce52d967542d2f3959691aff4fccfe32fd
theBlackWolf7411 <tanbqhedspik55@gmail.com>
2014-01-09 09:24:51 +0700

    Add author's comment

--- ArtCameraPro/src/hust/team7/actionfilter/GrayscaleFilterAction.java
+++ ArtCameraPro/src/hust/team7/actionfilter/GrayscaleFilterAction.java
@@ -10,13 +10,23 @@ public class GrayscaleFilterAction {
 	private int width;
 	private int height;
 	private int[] colors;
-
+	
+	/**
+	Constructor GrayscaleFilterAction
+	@param bmp
+	@author 7-A Bui Quang Tan
+	*/
 	public GrayscaleFilterAction(Bitmap bmp) {
 		this.bmp = bmp;
 		this.width = bmp.getWidth();
 		this.height = bmp.getHeight();
 	}
-
+	
+	/**
+	Handler Grayscale Effect
+	@param no param
+	@author 7-A Bui Quang Tan
+	*/
 	public Bitmap action() {
 		colors = AndroidUtils.bitmapToIntArray(this.bmp);
 		GrayscaleFilter filter = new GrayscaleFilter();
