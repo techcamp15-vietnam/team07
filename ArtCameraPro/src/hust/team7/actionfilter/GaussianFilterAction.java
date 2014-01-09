e0844fc9505b837ffec321eb1e2fc0327ca57410
theBlackWolf7411 <tanbqhedspik55@gmail.com>
2014-01-09 09:23:51 +0700

    Add author's comment

--- ArtCameraPro/src/hust/team7/actionfilter/GaussianFilterAction.java
+++ ArtCameraPro/src/hust/team7/actionfilter/GaussianFilterAction.java
@@ -1,26 +1,38 @@
 package hust.team7.actionfilter;
 
-import android.graphics.Bitmap;
-import hust.team7.filter.BlurFilter;
 import hust.team7.filter.GaussianFilter;
-import hust.team7.filter.util.*;
+import hust.team7.filter.util.AndroidUtils;
+import android.graphics.Bitmap;
 
 public class GaussianFilterAction {
 	private Bitmap bmp;
-
+	private int height;
+	private int width;
+	
+	/**
+	Constructor GaussianFilterAction
+	@param bmp
+	@author 7-A Bui Quang Tan
+	*/
 	public GaussianFilterAction(Bitmap bmp) {
 		this.bmp = bmp;
-	}
+		this.height = bmp.getHeight();
+		this.width = bmp.getWidth(); 
 
+	}
+	
+	/**
+	Handler Gaussian Effect
+	@param no param
+	@author 7-A Bui Quang Tan
+	*/
 	public Bitmap action() {
-		int height = bmp.getHeight();
-		int width = bmp.getWidth();
 
 		int[] colors = AndroidUtils.bitmapToIntArray(bmp);
 		GaussianFilter gaussFilter = new GaussianFilter();
-		
+
 		gaussFilter.setRadius(5);
-		
+
 		colors = gaussFilter.filter(colors, width, height);
 		bmp = Bitmap.createBitmap(colors, 0, width, width, height,
 				Bitmap.Config.ARGB_8888);
