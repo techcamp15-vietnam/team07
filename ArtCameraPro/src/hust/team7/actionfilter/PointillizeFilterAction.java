9b7d4158cce92f3cd3085414818393035cc90a26
theBlackWolf7411 <tanbqhedspik55@gmail.com>
2014-01-09 09:26:35 +0700

    Handler pointillize effect

--- ArtCameraPro/src/hust/team7/actionfilter/PointillizeFilterAction.java
+++ ArtCameraPro/src/hust/team7/actionfilter/PointillizeFilterAction.java
@@ -1,30 +1,42 @@
 package hust.team7.actionfilter;
 
+
 import hust.team7.filter.PointillizeFilter;
 import hust.team7.filter.util.AndroidUtils;
 import android.graphics.Bitmap;
 import android.graphics.Color;
-import android.widget.ImageView;
 
 public class PointillizeFilterAction {
+
 	private Bitmap bmp;
 	private int width;
 	private int height;
-	private float sizeValue = 7f;
-	private float randomnessValue = 0.25f;
-	private float fuzzinessValue = 0.05f;
 	private int[] colors;
-
-	// private ImageView ivEditImage;
-
-	public PointillizeFilterAction(Bitmap bmp, ImageView ivEditImage) {
+	private int sizeValue;
+	private float randomnessValue;
+	private float fuzzinessValue;
+	
+	/**
+	Constructor PointillizeFilterAction
+	@param bmp, sizeValue, randomnessValue, fuzzinessValue
+	@author 7-A Bui Quang Tan
+	*/
+	public PointillizeFilterAction(Bitmap bmp, int sizeValue, float randomnessValue, float fuzzinessValue) {
 		this.bmp = bmp;
-		this.width = ivEditImage.getDrawable().getIntrinsicWidth();
-		this.height = ivEditImage.getDrawable().getIntrinsicHeight();
-		colors = AndroidUtils.drawableToIntArray(ivEditImage.getDrawable());
+		this.width = bmp.getWidth();
+		this.height = bmp.getHeight();
+		this.sizeValue = sizeValue;
+		this.randomnessValue = randomnessValue;
+		this.fuzzinessValue = fuzzinessValue;
 	}
-
+	
+	/**
+	Handler Pointillize Effect
+	@param no param
+	@author 7-A Bui Quang Tan
+	*/
 	public Bitmap action() {
+		colors = AndroidUtils.bitmapToIntArray(this.bmp);
 		PointillizeFilter filter = new PointillizeFilter();
 		filter.setEdgeColor(Color.BLACK);
 		filter.setScale(sizeValue);
@@ -37,20 +49,9 @@ public class PointillizeFilterAction {
 				Bitmap.Config.ARGB_8888);
 		return bmp;
 
-		/*
-		 * new Thread(new Runnable() {
-		 * 
-		 * @Override public void run() { PointillizeFilter filter = new
-		 * PointillizeFilter(); filter.setEdgeColor(Color.BLACK);
-		 * filter.setScale(sizeValue); filter.setRandomness(randomnessValue);
-		 * filter.setAmount(0); filter.setFuzziness(fuzzinessValue);
-		 * filter.setGridType(PointillizeFilter.HEXAGONAL); colors =
-		 * filter.filter(colors, width, height); bmp =
-		 * Bitmap.createBitmap(colors, 0, width, width, height,
-		 * Bitmap.Config.ARGB_8888); ivEditImage.setImageBitmap(bmp); }
-		 * }).start();
-		 */
-
 	}
 
 }
+
+
+
